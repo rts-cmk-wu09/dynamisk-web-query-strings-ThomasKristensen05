3e279ddb10a290c41316be60d60ca30f2e7ac8dc
ThomasKristensen05 <thomas12andre@gmail.com>
2023-02-27 13:54:32 +0100

    alle json filerne er fetched

---
+++ app.js
@@ -0,0 +1,61 @@
+let main = document.querySelector("main")
+
+fetch("/data/1.json")
+.then(response => response.json())
+.then(data => {
+    console.log(data)
+
+    
+
+    })
+
+fetch("/data/2.json")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+
+    })
+
+fetch("/data/3.json")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+
+    })
+
+fetch("/data/4.json")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+
+    })
+
+fetch("/data/5.json")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+
+    })
+
+fetch("/data/6.json")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+
+    })
+
+fetch("/data/7.json")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+
+    })
+
+fetch("/data/8.json")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+
+    })
+    
+    
