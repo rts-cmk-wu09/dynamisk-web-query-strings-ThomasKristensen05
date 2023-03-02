1e8d38abe7d7abd6c891810a7c441c821fd79bf7
ThomasKristensen05 <thomas12andre@gmail.com>
2023-03-02 10:57:20 +0100

    prøvede mig bare frem

--- app.js
+++ app.js
@@ -1,61 +1,32 @@
-let main = document.querySelector("main")
+const destinationsList = document.querySelector(".destinationsList")
+const destinations = document.querySelector("destinations.json")
 
-fetch("/data/1.json")
+fetch("/data/destinations.json")
 .then(response => response.json())
 .then(data => {
     console.log(data)
+    data.destinations.forEach(destination => {
+        destinationsList.innerHTML += `
+        <figure id="${destinations.id}">
+            <img src="${destinations.id}>
+            <div>
+                <span><i class="fa-solid fa-heart"></i></span>
+                <a href="/destinations.html?id=${destinations.id}">More</a>
+            </div>
+        </figure>
+        `
+    })
 
-    
+        // main.innerHTML = `
+        
+        //     <img src="${data.image}" />        
+        //     <p class="destination">${data.destination}</p>        
+        //     <p class="title">${data.title}</p>        
+        //     <p class="subtitle">${data.subtitle}</p>        
+        //     <p class="text">${data.text}</p>        
+        // `
+
+        
 
     })
 
-fetch("/data/2.json")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-
-    })
-
-fetch("/data/3.json")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-
-    })
-
-fetch("/data/4.json")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-
-    })
-
-fetch("/data/5.json")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-
-    })
-
-fetch("/data/6.json")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-
-    })
-
-fetch("/data/7.json")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-
-    })
-
-fetch("/data/8.json")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-
-    })
-    
-    
